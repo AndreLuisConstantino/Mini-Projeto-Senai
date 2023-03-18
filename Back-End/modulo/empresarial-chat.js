872bb99eecf3eeb4038e4c973c10a161b875b5c8
André Luiz Constantino <andreluis081205@gmail.com>
2023-03-18 16:31:55 -0300

    Funções prontas

--- Back-End/modulo/empresarial-chat.js
+++ Back-End/modulo/empresarial-chat.js
@@ -6,21 +6,40 @@
  * Versão: 1.0 
  *****************************************************************************/
 
-var arquivoContatosConversas = require('./contatos.js')
+var listaContatos = require('./contatos.js')
+
+var arquivoContatosConversas = listaContatos.contatos['whats-users']
 
 const getContato = function (numeroDoContato) {
-    let jsonListaDeConversa = {}
+    let contatos = []
     let status = false
-    let conversa = []
+    let arrayMessages = []
+    let jsonUser = {}
 
-    arquivoContatosConversas.contatos['whats-users'].forEach(function (usuario) {
-        if (arquivoContatosConversas.contatos['whats-users'].number == numeroDoContato) {
+
+    arquivoContatosConversas.forEach(function (usuario) {
+
+        // console.log('teste')
+        if (usuario.number == numeroDoContato) {
+            // console.log('teste')
             usuario.contacts.forEach(function (contato) {
+                let jsonListaDeConversa = {}
                 jsonListaDeConversa.name = contato.name
                 jsonListaDeConversa.description = contato.description
                 jsonListaDeConversa.image = contato.image
-            })
+                arrayMessages = contato.messages
+                jsonListaDeConversa.messages = arrayMessages
 
+                contatos.push(jsonListaDeConversa)
+            })
         }
+        status = true
     })
+    if (status == true) {
+        return contatos
+    } else {
+        return status
+    }
 }
+
+console.log(getContato('11966578996'))
